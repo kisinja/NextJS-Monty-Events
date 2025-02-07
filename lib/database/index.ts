8a6fd63d500feb021ee062b0ac63d9a251f33a2c
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2025-02-07 19:39:00 +0300

    fixed the database connection

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
