3bf9d86f91ba998d942bde0a9391f3fdd24d55b7
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2025-02-07 21:27:08 +0300

    commit

---
+++ app/(root)/events/create/page.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import EventForm from '@/components/shared/EventForm';
+import { useUser } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs/server';
+import React from 'react'
+
+const CreateEvent = () => {
+
+    /* const { sessionClaims } = auth();
+    console.log(sessionClaims); */
+    const { user } = useUser();
+    const userId = user?.id as string;
+
+    return (
+        <>
+            <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
+                <h3 className='wrapper h3-bold text-center sm:text-left'>Create Event</h3>
+            </section>
+            <div className="wrapper my-8">
+                <EventForm userId={userId} type="Create" />
+            </div>
+        </>
+    )
+}
+
+export default CreateEvent;
---
+++ lib/validator.ts
@@ -0,0 +1,14 @@
+import * as z from 'zod';
+
+export const eventFormSchema = z.object({
+    title: z.string().min(3, "Title must be at least 3 characters long"),
+    description: z.string().min(10, "Description must be at least 10 characters long").max(400, "Description must be at most 400 characters long"),
+    location: z.string().min(3, "Location must be at least 3 characters long").max(400, "Location must be at most 400 characters long"),
+    imageUrl: z.string().url("Invalid URL"),
+    startDateTime: z.date(),
+    endDateTime: z.date(),
+    category: z.string().min(3, "Category must be at least 3 characters long").max(30, "Category must be at most 30 characters long"),
+    price: z.string(),
+    isFree: z.boolean(),
+    url: z.string().url("Invalid URL"),
+});
