561fc00eb74457d07190fe565382f4dc607786dc
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2025-02-20 21:53:24 +0300

    commit

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -15,7 +15,7 @@ const EventDetails = async ({ params: { id } }: SearchParamProps) => {
                     src={event.imageUrl}
                     alt="hero image"
                     width={1000}
-                    height={1000}
+                    height={800}
                     className="h-full min-h-[300px] object-cover object-center"
                 />
 
@@ -26,7 +26,7 @@ const EventDetails = async ({ params: { id } }: SearchParamProps) => {
                         <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
                             <div className="flex gap-3">
                                 <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">
-                                    {event.isFree ? 'FREE' : `$${event.price}`}
+                                    {event.isFree ? 'FREE' : `KES${event.price}`}
                                 </p>
                                 <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-grey-500">
                                     {event.category.name}
@@ -43,16 +43,15 @@ const EventDetails = async ({ params: { id } }: SearchParamProps) => {
                     </div>
 
                     <div className="flex flex-col gap-5">
-                        <div className="flex-gap-2 md:gap-3">
+                        <div className="flex gap-2 md:gap-3">
                             <Image src="/assets/icons/calendar.svg" width={32} height={32} alt="icon" />
                             <div className="p-medium-16 lg:p-regular-20 flex flex-wrap items-center">
                                 <p>
-                                    {formatDateTime(event.startDateTime).dateOnly} - {formatDateTime(event.startDateTime).timeOnly}
+                                    {formatDateTime(event.startDateTime).dateOnly} - {' '} {formatDateTime(event.startDateTime).timeOnly}
                                 </p>
                                 <p className="ml-1">
-                                    {formatDateTime(event.endDateTime).dateOnly} - {formatDateTime(event.endDateTime).timeOnly}
+                                    {formatDateTime(event.endDateTime).dateOnly} - {' '} {formatDateTime(event.endDateTime).timeOnly}
                                 </p>
-
                             </div>
                         </div>
 
@@ -63,7 +62,7 @@ const EventDetails = async ({ params: { id } }: SearchParamProps) => {
                     </div>
 
                     <div className="flex flex-col gap-2">
-                        <p className="p-bold-20 text-gray-600">What you'll Learn</p>
+                        <p className="p-bold-20 text-gray-600">What you'll Learn:</p>
                         <p className="p-medium-16 lg:p-regular-18">{event.description}</p>
                         <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 underline">{event.url}</p>
                     </div>
